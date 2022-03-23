d4b9586b0e72c50476eb7dc9317c87206c0ad864
ari <arifwidayana@gmail.com>
2022-03-23 15:35:22 +0700

    add new text and image in ThirdScreenFragment.kt and fixing new layout and function

--- app/src/main/java/com/arifwidayana/challangechapter3/view/ThirdScreenFragment.kt
+++ app/src/main/java/com/arifwidayana/challangechapter3/view/ThirdScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.arifwidayana.challangechapter3.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -23,6 +24,7 @@ class ThirdScreenFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Pass data with bundle
@@ -32,15 +34,22 @@ class ThirdScreenFragment : Fragment() {
         val value = arguments?.getParcelable<PriceBookValue>(FourthScreenFragment.VALUE)
         when {
             name != null -> {
+                binding.ivPbv.visibility = View.GONE
                 binding.tvResultCalculate.visibility = View.GONE
-                binding.tvName.text = name
+                binding.tvEquity.visibility = View.GONE
+                binding.tvShare.visibility = View.GONE
+                binding.tvPriceShare.visibility = View.GONE
+                binding.tvName.text = "Selamat datang $name"
             }
             else -> {
                 val bookValue = value?.equity.toString().toDouble() / value?.share.toString().toDouble()
                 val result = value?.priceShare.toString().toDouble() / bookValue
                 binding.tvName.visibility = View.GONE
                 binding.btnToScreen4.visibility = View.GONE
-                binding.tvResultCalculate.text = result.toString()
+                binding.tvEquity.text = "Ekuitas Perusahaan = ${value?.equity}"
+                binding.tvShare.text = "Lembar Saham Beredar = ${value?.share}"
+                binding.tvPriceShare.text = "Harga Lembar Saham = ${value?.priceShare}"
+                binding.tvResultCalculate.text = "Hasil Perhitungan PBV adalah $result`x "
             }
         }
 
