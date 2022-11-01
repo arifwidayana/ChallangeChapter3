58a47ea00ad192f986c12abec437304b8c4983ab
Arif Widayana <arifwidayana@gmail.com>
2022-11-01 12:19:55 +0700

    [UPDATED] private to protected class

--- app/src/main/java/com/arifwidayana/challangechapter3/common/BaseFragment.kt
+++ app/src/main/java/com/arifwidayana/challangechapter3/common/BaseFragment.kt
@@ -11,7 +11,7 @@ import com.google.android.material.snackbar.Snackbar
 abstract class BaseFragment<VB: ViewBinding>(
     private val bindingInflater: (LayoutInflater) -> VB
 ): Fragment(), BaseView{
-    private lateinit var binding : VB
+    protected lateinit var binding : VB
 
     override fun onCreateView(
         inflater: LayoutInflater,
